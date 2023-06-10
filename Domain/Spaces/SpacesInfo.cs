dcef0f5ea964a923de4dee761de54658089d56a8
Ilya <ividivi67@gmail.com>
2023-06-10 21:25:29 +0300

    feat: make SpacesInfo class, add SpaceNumberAdd(int, int)

--- Domain/Spaces/SpacesInfo.cs
+++ Domain/Spaces/SpacesInfo.cs
@@ -1,3 +1,24 @@
 ﻿namespace Domain.Spaces;
 
-public record SpacesInfo(int[,] Mask, int Count);
+public class SpacesInfo
+{
+    private readonly int[,] _mask;
+
+    public SpacesInfo(int[,] mask, int count)
+    {
+        _mask = mask;
+        Count = count;
+    }
+
+    public int Count { get; }
+
+    public int SpaceNumberAt(int i, int j)
+    {
+        if (i < 0 || j < 0 || i >= _mask.GetLength(0) || j >= _mask.GetLength(1))
+        {
+            throw new NotImplementedException();
+        }
+
+        return _mask[i, j];
+    }
+}
