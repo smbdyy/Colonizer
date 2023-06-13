b8e1d15530d8e1d034ff9ab5c49dd7598703a208
Ilya <ividivi67@gmail.com>
2023-06-13 16:48:01 +0300

    feat: add Thread.Sleep, increase window size

--- Colonizer.ConsoleApp/Program.cs
+++ Colonizer.ConsoleApp/Program.cs
@@ -4,16 +4,16 @@ using Domain.Fields;
 var size = new FieldSize(30, 30);
 var field = new Field(size);
 
-var colors = new[] { ConsoleColor.Blue, ConsoleColor.White, ConsoleColor.Red };
+var colors = new[] { ConsoleColor.Blue, ConsoleColor.Green, ConsoleColor.Red };
 field.FillWithRandomColorsFromPool(colors);
 field.RecalculateSpaces();
 
 var drawer = new FieldDrawer();
 Console.Clear();
-Console.SetWindowSize(size.Width, size.Height);
+Console.SetWindowSize(size.Width + 5, size.Height + 5);
 while (true)
 {
     drawer.Draw(field);
     field.Mutate();
-    Console.ReadKey();
+    Thread.Sleep(400);
 }
